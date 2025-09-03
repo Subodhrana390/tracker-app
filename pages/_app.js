cbdbfce3c3cd43b5c9f6cf4aaf01a4b76e9f7c2c
Subodh Rana <subodhrana390@gmail.com>
2025-09-04 00:42:32 +0530

    added Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "@/styles/globals.css";
 import { UserProvider } from "../context/UserContext";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <Component {...pageProps} />
     </UserProvider>
   );
